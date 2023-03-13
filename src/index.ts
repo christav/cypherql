c14378d9793ea0bc326a246f77443e81bfaf6587
Chris Tavares <cct@tavaresstudios.com>
2023-03-12 20:26:02 -0700

    Fastify install and basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log('Hello from typescript, being watched!');
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
+import { Server, IncomingMessage, ServerResponse } from 'http';
+
+const server = Fastify({});
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          pong: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
+}
+
+server.get('/ping', opts, async (req, reply) => {
+  return { pong: 'It works!' };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+
+    const address = server.server.address();
+    const port = typeof address === 'string' ? address : address?.port;
+    console.log(`Server listening on http://localhost/${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
